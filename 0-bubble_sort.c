0927fa52633e01d8ba2583740269a9f23151c87e
Stacy-Muhia <stacymuhiap@gmail.com>
2023-09-25 09:22:27 -0700

    first step

---
+++ 0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * bubble_sort -  sorts an array of integers using bubble sort algorithm
+ * @array: the integers to be sorted using the bubbl sort algorithm
+ * @size: the propotions of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+
