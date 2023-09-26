692a3b81e136856abcddebe81aea7fa8e41d247c
Stacy-Muhia <stacymuhiap@gmail.com>
2023-09-26 13:40:20 -0700

    wuehh

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * quick_sort -  functions to sort array of integers in ascending order
+ * using the quick sort algorithm
+ * @array: The integers to be sorted using the quick sort algorithm
+ * @size:  the propotions of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+
--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void bubble_sort(int *array, size_t size);
 void node_swp(listint_t **head, listint_t **node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
