37a18a545eadbd7eecda8da6eadc92fadd2f9950
Stacy-Muhia <stacymuhiap@gmail.com>
2023-09-26 10:33:35 -0700

    remaining partof task two

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,4 +7,28 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t tmp, idx, min;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (tmp = 0; tmp < size - 1; tmp++)
+	{
+		min = tmp;
+
+		for (idx = tmp + 1; idx < size; idx++)
+		{
+			if (array[idx] < array[min])
+				min = idx;
+		}
+
+		if (min != tmp)
+		{
+			temp = array[tmp];
+			array[tmp] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
 }
